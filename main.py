682758c341f8cbd27b9e48c8f819462a4688d70f
Sangy-kun <rakotofif6@gmail.com>
2025-09-02 17:32:21 +0300

    ajout de ping et les environemen necessaire

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+app =FastAPI()
+from starlette.responses import PlainTextResponse
+
+@app.get("/ping", response_class=PlainTextResponse, status_code=200)
+async def get_ping():
+    return "pong"
