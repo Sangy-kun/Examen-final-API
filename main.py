400fa6b221f977333a85db5daa1d4e38cd1436dc
Sangy-kun <rakotofif6@gmail.com>
2025-09-03 09:09:03 +0300

    ajout de get health

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ from starlette.responses import PlainTextResponse
 @app.get("/ping", response_class=PlainTextResponse, status_code=200)
 async def get_ping():
     return "pong"
+
+@app.get("/health")
+async def get_health():
+    return "Ok"
