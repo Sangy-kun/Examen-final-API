b6d849c6604c8ab907cf93eb95942aa85753a939
Sangy-kun <rakotofif6@gmail.com>
2025-09-03 09:58:32 +0300

    ajout de post phone et get phone + get phone par id

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app =FastAPI()
-from starlette.responses import PlainTextResponse
+from starlette.responses import PlainTextResponse, JSONResponse
+
 
 @app.get("/ping", response_class=PlainTextResponse, status_code=200)
 async def get_ping():
@@ -9,3 +13,31 @@ async def get_ping():
 @app.get("/health")
 async def get_health():
     return "Ok"
+
+class Characteristic(BaseModel):
+    ram_memory: int
+    rom_memory: int
+
+class Phone(BaseModel):
+    id: str
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+phones_db: List[Phone] = []
+@app.post("/phones", response_class=JSONResponse, status_code=201)
+async def post_phone(phone: Phone):
+    phones_db.append(phone)
+    return phone
+
+@app.get("/phones")
+async def get_phones():
+    return phones_db
+
+
+@app.get("/phones/{id}")
+async def get_phone(id: str):
+    for phone in phones_db:
+        if phone.id == id:
+            return phone
+    return {"error": "le phone comportant l'id fourni n'existe pas"}
